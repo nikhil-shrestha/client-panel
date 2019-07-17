f8b0f8ae33b13e286908c9ed883503d2f351bd30
Nikhil Shrestha <nikhil.shrestha1995@gmail.com>
2019-07-17 12:50:40 +0545

    notify state & alert component

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-// import { compose } from 'redux';
-// import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
 import { firebaseConnect } from 'react-redux-firebase';
 import PropTypes from 'prop-types';
-
+import { notifyUser } from '../../actions/notifyAction';
 import TextInputGroup from './../layout/TextInputGroup';
+import Alert from './../layout/Alert';
 
 class Login extends Component {
   state = {
@@ -16,22 +17,26 @@ class Login extends Component {
   onSubmit = e => {
     e.preventDefault();
 
-    const { firebase } = this.props;
+    const { firebase, notifyUser } = this.props;
     const { email, password } = this.state;
     firebase
       .login({
         email,
         password
       })
-      .catch(err => alert('Invalid Login Credentials'));
+      .catch(err => notifyUser('Invalid Login Credentials', 'error'));
   };
 
   render() {
+    const { message, messageType } = this.props.notify;
     return (
       <div className="row">
         <div className="col-md-6 mx-auto">
           <div className="card">
             <div className="card-body">
+              {message ? (
+                <Alert message={message} messageType={messageType} />
+              ) : null}
               <h1 className="text-center pb-4 pt-3">
                 <span className="text-primary">
                   <i className="fas fa-lock" /> Login
@@ -74,4 +79,12 @@ Login.propTypes = {
   firebase: PropTypes.object.isRequired
 };
 
-export default firebaseConnect()(Login);
+export default compose(
+  firebaseConnect(),
+  connect(
+    (state, props) => ({
+      notify: state.notify
+    }),
+    { notifyUser }
+  )
+)(Login);
--- src/store.js
+++ src/store.js
@@ -5,7 +5,7 @@ import { reactReduxFirebase, firebaseReducer } from 'react-redux-firebase';
 import { reduxFirestore, firestoreReducer } from 'redux-firestore';
 
 // Reucers
-// @todo
+import notifyReducer from './reducers/notifyReducer';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyCm0jFukfMccXjhj6e8-VdMhrh9ZB9XhMM',
@@ -38,7 +38,8 @@ const createStoreWithFirebase = compose(
 // Add firebase to reducers
 const rootReducer = combineReducers({
   firebase: firebaseReducer,
-  firestore: firestoreReducer // <- needed if using firestore
+  firestore: firestoreReducer,
+  notify: notifyReducer
 });
 
 // Create initial state
