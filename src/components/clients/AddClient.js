4fd3172d5d19150acffc8d744c22c5d16d42e345
Nikhil Shrestha <nikhil.shrestha1995@gmail.com>
2019-07-17 08:35:09 +0545

    add client to firestore

---
+++ src/components/clients/AddClient.js
@@ -0,0 +1,111 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+// import { compose } from 'redux';
+// import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import PropTypes from 'prop-types';
+
+import TextInputGroup from './../layout/TextInputGroup';
+
+class AddClient extends Component {
+  state = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    balance: ''
+  };
+
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  onSubmit = e => {
+    e.preventDefault();
+    const newClient = this.state;
+
+    // if no balance, make 0
+
+    if (newClient.balance === '') {
+      newClient.balance = 0;
+    }
+
+    const { firestore, history } = this.props;
+    firestore
+      .add({ collection: 'clients' }, newClient)
+      .then(() => history.push('/'));
+  };
+
+  render() {
+    const { firstName, lastName, email, phone, balance, errors } = this.state;
+    return (
+      <div>
+        <div className="row">
+          <div className="col-md-6">
+            <Link to="/" className="btn btn-link">
+              <i className="fas fa-arrow-circle-left" /> Back To Dashboard
+            </Link>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="card-header">Add Client</div>
+          <div className="card-body">
+            <form onSubmit={this.onSubmit}>
+              <TextInputGroup
+                label="First Name"
+                name="firstName"
+                placeholder="Enter First Name"
+                value={firstName}
+                minLength="2"
+                onChange={this.onChange}
+                required
+              />
+              <TextInputGroup
+                label="Last Name"
+                name="lastName"
+                placeholder="Enter Last Name"
+                value={lastName}
+                minLength="2"
+                onChange={this.onChange}
+                required
+              />
+              <TextInputGroup
+                label="Email"
+                name="email"
+                type="email"
+                placeholder="Enter Email"
+                value={email}
+                onChange={this.onChange}
+              />
+              <TextInputGroup
+                label="Phone"
+                name="phone"
+                placeholder="Enter Phone Number"
+                value={phone}
+                minLength="10"
+                onChange={this.onChange}
+              />
+              <TextInputGroup
+                label="Balance"
+                name="balance"
+                placeholder="Enter Balance"
+                value={balance}
+                onChange={this.onChange}
+              />
+              <input
+                type="submit"
+                value="Submit"
+                className="btn btn-primary btn-block"
+              />
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+AddClient.propType = {
+  firestore: PropTypes.object.isRequired
+};
+
+export default firestoreConnect()(AddClient);
---
+++ src/components/layout/TextInputGroup.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const TextInputGroup = ({
+  label,
+  name,
+  value,
+  placeholder,
+  type,
+  onChange,
+  minLength,
+  required
+}) => {
+  return (
+    <div className="form-group">
+      <label htmlFor={name}>{label}</label>
+      <input
+        type={type}
+        name={name}
+        className="form-control"
+        placeholder={placeholder}
+        value={value}
+        minLength={minLength}
+        onChange={onChange}
+        required={required}
+      />
+    </div>
+  );
+};
+
+TextInputGroup.propTypes = {
+  label: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  placeholder: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired
+};
+
+TextInputGroup.defaultProps = {
+  type: 'text'
+};
+
+export default TextInputGroup;
